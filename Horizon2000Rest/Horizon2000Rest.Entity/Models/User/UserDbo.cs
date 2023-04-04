8f5bd944e7b7dbd1d9557e4139d7d60dc692e0d5
DonFalton <martin.blazquez.19@gmail.com>
2023-04-04 18:58:35 +0200

    Add files via upload
    Database Models

---
+++ Horizon2000Rest/Horizon2000Rest.Entity/Models/User/UserDbo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horizon2000Rest.Entity.Models.User
+{
+    public class UserDbo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public bool Active { get; set; }
+    }
+}
